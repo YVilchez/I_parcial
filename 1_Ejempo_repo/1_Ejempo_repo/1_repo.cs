74b17a5375b7b150437bcb417b44e6a4c50d6846
yohanna <yohannavilchez02@gmail.com>
2023-01-29 16:50:26 +0000

    Procedimientos/Funsiones

--- 1_Ejempo_repo/1_Ejempo_repo/1_repo.cs
+++ 1_Ejempo_repo/1_Ejempo_repo/1_repo.cs
@@ -15,17 +15,43 @@ namespace _1_Ejempo_repo
         public valo2()
         {
             InitializeComponent();
+
         }
+
+        decimal sumaTotal;
 
         private void rsuma_Click(object sender, EventArgs e)
         {
             //Variables
-            decimal numero1 = Convert.ToDecimal(valor1.Text) ;
-            decimal numero2 = Convert.ToDecimal(valor2.Text) ;
+           decimal numero1 = Convert.ToDecimal(valor1.Text) ;
+           decimal numero2 = Convert.ToDecimal(valor2.Text);
 
-            decimal resultado = numero1 + numero2;
+            //decimal resultado = numero1 + numero2;
 
-            MessageBox.Show("El resultado de la suma es: " + resultado);
+            //MessageBox.Show("El resultado de la suma es: " + resultado);
+
+            //LLAMAR PROCEDIMIENTO
+
+            sumaTotal=Sumatoria(numero1, numero2);
+
+            //LLAMAR FUNSION
+            Sumatoria(numero1, numero2);
+            //PARA mensajes se pueden usar botones 
+            MessageBox.Show("La suma es: " + sumaTotal, "Mensaje", MessageBoxButtons.OK, MessageBoxIcon.Information);
+        }
+
+        //PROCEDIMIENTOS
+        private void Sumar(decimal N1, decimal N2)
+        {
+            //Se declaro como vriabke global
+            sumaTotal = N1 + N2;
+
+        }
+
+        //FUNCION
+        private decimal Sumatoria(decimal N1, decimal N2)
+        {
+            return N1 + N2;
         }
     }
 }
