ebe1f876bb97fd2890e16b234872ba4d519679fc
yohanna <yohannavilchez02@gmail.com>
2023-01-30 19:12:01 +0000

    fechas

---
+++ 1_Ejempo_repo/1_Ejempo_repo/Ejercicio4.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace _1_Ejempo_repo
+{
+    public partial class Ejercicio4 : Form
+    {
+        public Ejercicio4()
+        {
+            InitializeComponent();
+        }
+
+        private void label3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label4_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
