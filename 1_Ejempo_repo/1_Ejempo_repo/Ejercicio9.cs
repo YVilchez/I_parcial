8c43360a8296707ab5b34d31388dab4d70f81042
yohanna <yohannavilchez02@gmail.com>
2023-03-01 00:23:42 +0000

    POO

---
+++ 1_Ejempo_repo/1_Ejempo_repo/Ejercicio9.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+//USAR 
+using POO;
+
+namespace _1_Ejempo_repo
+{
+    public partial class Ejercicio9 : Form
+    {
+        public Ejercicio9()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            //INSTANCIAR OBJETO  DE LA CLASE Coche
+            //Proyecto principal agregar Referencia
+            //Nombre de objeto siempre en minuscula
+
+            Coche miCoche = new Coche();
+            Coche miCoche2 = new Coche("Toyota", "22R");
+
+        }
+    }
+}
---
+++ 1_Ejempo_repo/POO/Coche.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace POO
+{
+   public class Coche
+    {
+        //ATRIBUTOS CARACTERISTICAS DE LA CLASE
+        //SIEMPRE PRIVADOS
+        private string marca;
+        private string modelo;
+        private decimal precio;
+
+        // PROPIEDADES (acceder a los atributos)
+        //SIEMPRE PUBLICOS
+        //SIEMPRE empieza en MAYUSCULA
+
+   //==================================================================
+        public string Marca
+        {
+            //Obtener
+            get { return marca; }
+            //mandar informacion al atributo
+            set { marca = value; }
+        }
+    //OTRAS FORMAS
+        public string Modelo { get => modelo; set => modelo = value; }
+        //MAS FACIL=
+        public decimal Precio { get; set; }
+    // prop + 2 veces tabulador
+        public int Kilometros { get; set; }
+
+
+    //=================================================================
+        //CONSTRUCTORES // Siempre publico y el mimo nombre de la clase
+        public Coche() { }
+
+        public Coche(string _marca, string _modelo) 
+        {
+            Marca = marca;
+            Modelo = modelo;
+
+        }
+
+        //ACCIONES RAPIDAS selecionar desarmador de lado,GENERAR CONSTRUCTOR, Seleccionar solo s propiedades
+        public Coche(string marca, string modelo, decimal precio, int kilometros) : this(marca, modelo)
+        {
+            Precio = precio;
+            Kilometros = kilometros;
+        }
+
+//======================================================================
+//METODOS, ACCIONES QUE VA HA REALIZAR, CALCULOS.
+
+        public string Devolver_DatosBasicos()
+        {
+            return "Marca " + Marca + "Modelo:" + Modelo + "Precio:" + Precio;
+
+                
+        }
+    }
+}
